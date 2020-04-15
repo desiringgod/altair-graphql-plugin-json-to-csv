3b8e6737afcb859c49f66798f02268ac59083820
Isaac Vazquez <isaachvazquez@gmail.com>
2020-04-15 14:42:00 -0500

    Test json-to-csv api and file download.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var FileSaver = require('file-saver');
+
 class ActionButtonJsonToCSV {
   constructor(props) {
     console.log('-----------');
@@ -18,6 +20,24 @@ class ActionButtonJsonToCSV {
     console.log('===========');
     console.log(props);
     console.log('===========');
+
+    const email = 'test@example.com';
+    const json = {
+      "test": true,
+      "test2": false
+    };
+    const jsonString = JSON.stringify(json);
+    const url = `https://json-csv.com/api/getcsv?email=${email}&json=${jsonString}`;
+
+    fetch(url, {
+      method: 'POST',
+    })
+    .then(async response => {
+      const r = await response.text();
+      console.log(r);
+      var blob = new Blob([r], { type: "text/json;charset=utf-8" });
+      FileSaver.saveAs(blob, "download.csv");
+    });
   }
 
   // Perform cleanups in this function
