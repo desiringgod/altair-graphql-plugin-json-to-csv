f7a3452ebd767ad24852628bff7e9102558ac4dc
Isaac Vazquez <isaachvazquez@gmail.com>
2020-04-21 10:08:17 -0500

    Add file save ability

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 // import FileSaver from 'file-saver';
 
 class ActionButtonJsonToCSV {
-  constructor(props) {
-    console.log('-----------');
-    console.log(props);
-    console.log('-----------');
-  }
+  constructor(props) {}
 
   // Called to retrieve the render information for the button.
   // Currently only the text of the button is required.
@@ -30,22 +26,25 @@ class ActionButtonJsonToCSV {
     const url = `https://json-csv.com/api/getcsv?email=${email}&json=${jsonString}`;
 
     fetch(url, {
-      method: 'POST',
-    })
-    .then(async response => {
-      const csvOutput = await response.text();
-      console.log({csvOutput});
-      // var blob = new Blob([csvOutput], { type: "text/json;charset=utf-8" });
-      // FileSaver.saveAs(blob, "download.csv");
-    });
+        method: 'POST',
+      })
+      .then(async response => {
+        const csvOutput = await response.text();
+        console.log({ csvOutput });
+
+        const element = document.createElement('a');
+        const filename = 'download.csv';
+        element.setAttribute('href', 'data:text/csv;charset=utf-8,' + encodeURIComponent(csvOutput));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+      });
   }
 
   // Perform cleanups in this function
-  async destroy(props) {
-    console.log('+++++++++++++++++');
-    console.log(props);
-    console.log('+++++++++++++++++');
-  }
+  async destroy(props) {}
 }
 
 // Add the class to the Altair plugins object
